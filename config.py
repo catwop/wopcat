71095ce67aa0a9455c7f3fe400244f82cb5750bd
catwop <jameqnd@icloud.com>
2026-09-23 23:17:39 +0500

    Update bot tokens and admin IDs in config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py — настройки обоих ботов
-BOT_TOKEN = "123456:ABC-main-bot"        # токен ОСНОВНОГО бота (клиенты)
-ADMIN_BOT_TOKEN = "123456:DEF-admin-bot" # токен АДМИН-БОТА (только ты)
-ADMIN_IDS = [123456789]                  # твои Telegram ID (кто имеет доступ к админке)
+BOT_TOKEN = "8783129047:AAHYR0a-yxrV_qcIsVZK0ij88-e-9LtX6k8"        # токен ОСНОВНОГО бота (клиенты)
+ADMIN_BOT_TOKEN = "8290497106:AAGVYzJRQjC-slLf9FKxxrLacgcktvU6BqM" # токен АДМИН-БОТА (только ты)
+ADMIN_IDS = [8996873568]                  # твои Telegram ID (кто имеет доступ к админке)
 
 DB_PATH = "shop.db"
 LTC_WALLET = "ltc1qxxxxxxxxxxxxxxxxxxxxxx"  # стартовый адрес, меняется в админ-боте
